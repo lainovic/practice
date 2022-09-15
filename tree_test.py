90e4d38c3a27bf435341571cbdd4633187ad7dd6
Marko Lainovic <marko.lainovic@gmail.com>
2022-09-15 12:00:35 +0200

    add tests
    Signed-off-by: Marko Lainovic <marko.lainovic@gmail.com>

--- tree_test.py
+++ tree_test.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import unittest
 
 from tree import TreeNode
@@ -14,9 +15,21 @@ class TestTree(unittest.TestCase):
         self.root.left.left.left = TreeNode(1)
         self.list = [5, 3, 6, 2, 4, None, None, 1]
 
-    def test(self):
+    def test_serialization(self):
         self.assertEqual(TreeNode.deserialize(
             TreeNode.serialize(self.root)), self.root)
+
+    def test_eq(self):
+        self.assertEqual(self.root, self.root)
+
+    def test_neq(self):
+        self.assertNotEqual(self.root, None)
+        foo = deepcopy(self.root)
+        foo.left.left.left.right = TreeNode(17)
+        self.assertNotEqual(self.root, foo)
+        foo.left.left.left.right = None
+        self.root.right.right = TreeNode(42)
+        self.assertNotEqual(self.root, foo)
 
 
 if __name__ == "__main__":
