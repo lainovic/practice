36d3587750c9209b5060fec397e0637781d800d6
Marko Lainovic <marko.lainovic@gmail.com>
2022-09-25 14:32:32 +0200

    Tiny refactors.

--- src/maxheap.py
+++ src/maxheap.py
@@ -6,13 +6,19 @@ T = TypeVar("T")
 
 
 class MaxHeap:
-    def __init__(self, capacity: int = 1, greater_than_or_equal_predicate: Callable[[T, T], bool] = None, negative: Callable[[T], T] = None) -> None:
+    def __init__(self,
+                 capacity: int = 1,
+                 greater_than_or_equal_predicate: Callable[[
+                     T, T], bool] = None,
+                 negative: Callable[[T], T] = None) -> None:
         """
-        :note: in case of user-derived values, users must provide the callback for negating the values. Leaky abstractions, I know...
+        :note: in case of user-derived values, users must provide the callback for negating the values.
+        Leaky abstractions, I know...
         """
         self.negation_op = negative if negative else lambda x: (-1) * x
-        self.minheap = MinHeap(capacity, None if greater_than_or_equal_predicate is None else lambda x,
-                               y: not greater_than_or_equal_predicate(x, y))
+        self.minheap = MinHeap(capacity,
+                               less_than_predicate=None if greater_than_or_equal_predicate is None else
+                               lambda x, y: not greater_than_or_equal_predicate(x, y))
 
     def empty(self) -> bool:
         return self.minheap.empty()
@@ -22,14 +28,16 @@ class MaxHeap:
 
     def heapify(self, values, greater_than_or_equal_predicate: Callable[[T, T], bool] = None, negative: Callable[[T], T] = None) -> None:
         """
-        :note: in case of user-derived values, users must provide the callback for negating the values. Leaky abstractions, I know...
+        :note: in case of user-derived values, users must provide the callback for negating the values.
+        Leaky abstractions, I know...
         """
         if negative:
             self.negation_op = negative
         for i in range(len(values)):
             values[i] = self.negation_op(values[i])
-        self.minheap.heapify(values, None if greater_than_or_equal_predicate is None else lambda x,
-                             y: not greater_than_or_equal_predicate(x, y))
+        self.minheap.heapify(values,
+                             less_than_predicate=None if greater_than_or_equal_predicate is None else
+                             lambda x, y: not greater_than_or_equal_predicate(x, y))
 
     def max(self) -> T:
         return self.negation_op(self.minheap.min())
