a00598a2527d9eb80ba338d961d06b8ef5aef054
Marko Lainovic <marko.lainovic@gmail.com>
2022-09-25 12:04:51 +0200

    Extend MaxHeap to accept the custom compare predicate (greater than or equal), as well as the callback for negation of values. Leaky abstractions, yeah yeah...

--- src/maxheap.py
+++ src/maxheap.py
@@ -1,26 +1,47 @@
+from ast import Call
+from tkinter import N
+from tkinter.messagebox import NO
+from typing import Callable, TypeVar
 from src.minheap import MinHeap
+from operator import __ge__
+
+T = TypeVar("T")
 
 
 class MaxHeap:
-    def __init__(self):
-        self.minheap = MinHeap()
+    def __init__(self, capacity: int = 1, greater_than_or_equal_predicate: Callable[[T, T], bool] = None, negative: Callable[[T], T]=None) -> None:
+        """
+        :note: in case of user-derived values, users must provide the callback for negating the values. Leaky abstractions, I know...
+        """
+        less_than_predicate = None
+        if greater_than_or_equal_predicate is not None:
+            less_than_predicate = lambda x, y: not greater_than_or_equal_predicate(x, y)
+        self.negation_op = negative if negative is not None else lambda x: self.negation_op(x)
+        self.minheap = MinHeap(capacity, less_than_predicate)
 
-    def empty(self):
+    def empty(self) -> bool:
         return self.minheap.empty()
 
-    def size(self):
+    def size(self) -> int:
         return self.minheap.size()
 
-    def heapify(self, nums):
-        for i in range(len(nums)):
-            nums[i] = (-1) * nums[i]
-        self.minheap.heapify(nums)
+    def heapify(self, values, greater_than_or_equal_predicate: Callable[[T, T], bool] = None, negative: Callable[[T], T]=None) -> None:
+        """
+        :note: in case of user-derived values, users must provide the callback for negating the values. Leaky abstractions, I know...
+        """
+        less_than_predicate = None
+        if greater_than_or_equal_predicate is not None:
+            less_than_predicate = lambda x, y: not greater_than_or_equal_predicate(x, y)
+        self.negation_op = negative if negative is not None else lambda x: (-1) * x
+        for i in range(len(values)):
+            values[i] = self.negation_op(values[i])
+        self.minheap.heapify(values, less_than_predicate)
 
-    def max(self):
-        return (-1) * self.minheap.min()
+    def max(self) -> T:
+        return self.negation_op(self.minheap.min())
 
-    def extract_max(self):
-        return (-1) * self.minheap.extract_min()
+    def extract_max(self) -> T:
+        return self.negation_op(self.minheap.extract_min())
 
-    def insert(self, value):
-        self.minheap.insert((-1) * value)
+    def insert(self, value: T) -> None:
+        self.minheap.insert(self.negation_op(value))
--- test/test_maxheap.py
+++ test/test_maxheap.py
@@ -2,6 +2,7 @@ import unittest
 
 from src.maxheap import MaxHeap
 
+
 class MaxHeapTest(unittest.TestCase):
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName)
@@ -40,6 +41,34 @@ class MaxHeapTest(unittest.TestCase):
         self.insert_and_verify_max(mh, -4, -2)
         self.insert_and_verify_max(mh, 0, 0)
         self.insert_and_verify_max(mh, 4, 4)
+
+    def test_with_custom_predicate(self):
+        mh = MaxHeap()
+        mh.heapify([(0, 9), (1, 8), (2, 7), (3, 7), (4, 6), (5, 12)],
+                   greater_than_or_equal_predicate=lambda x, y: x[0] >= y[0],
+                   negative=lambda x : ((-1) * x[0], x[1]))
+        self.assertFalse(mh.empty())
+        self.assertEqual(mh.size(), 6)
+        self.assertEqual(mh.extract_max(), (5, 12))
+        self.assertEqual(mh.size(), 5)
+        self.assertEqual(mh.extract_max(), (4, 6))
+        self.assertEqual(mh.size(), 4)
+        self.assertEqual(mh.extract_max(), (3, 7))
+        self.assertEqual(mh.size(), 3)
+        self.assertEqual(mh.extract_max(), (2, 7))
+        self.assertEqual(mh.size(), 2)
+        mh.insert((10, 10))
+        self.assertEqual(mh.size(), 3)
+        self.assertEqual(mh.extract_max(), (10, 10))
+        self.assertEqual(mh.size(), 2)
+        mh.insert((1, 1))
+        self.assertEqual(mh.size(), 3)
+        self.assertEqual(mh.extract_max(), (1, 8))
+        self.assertEqual(mh.size(), 2)
+        self.assertEqual(mh.extract_max(), (1, 1))
+        self.assertEqual(mh.size(), 1)
+        self.assertEqual(mh.extract_max(), (0, 9))
+        self.assertTrue(mh.empty())
 
 
 if __name__ == "__main__":
